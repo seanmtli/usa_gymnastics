b30897fa5eef9bac933f96d00e0b63f6fd844588
benthorp33 <bht9@duke.edu>
2024-01-05 13:10:23 -0500

    Added reactive thing

--- App-1/server.R
+++ App-1/server.R
@@ -6,8 +6,17 @@ men <- read.csv("data/expected_medals/m_total_probabilities.csv")
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
+  selected_data <- reactive({
+    print(input$var)  # Debugging line
+    if (input$var == "Men") {
+      return(men)
+    } else {
+      return(women)
+    }
+  })
+  
   output$individuals <- renderDT(
-    women
+    selected_data()   
   )
   
 }
