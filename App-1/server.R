14afab05610483d52ab89ee41a32d006be5095f4
Sean Li <seanmtli@gmail.com>
2024-01-11 18:24:45 -0500

    starting to get team builder done

--- App-1/server.R
+++ App-1/server.R
@@ -5,6 +5,7 @@ w_topcand <- read.csv("data/expected_medals/f_total_probabilities.csv")
 m_topcand <- read.csv("data/expected_medals/m_total_probabilities.csv")
 m_byevent <- read.csv("data/expected_medals/usa_mens_avs.csv")
 w_byevent <- read.csv("data/expected_medals/usa_womens_avs.csv")
+
 # Define server logic for slider examples ----
 server <- function(input, output) {
   
@@ -32,16 +33,75 @@ server <- function(input, output) {
       m_byevent
     }
   })
+  ## Reactive to Mens/Womens on Team Builder Page
+  events_data2 <- reactive({
+    if (input$genderTeamBuilder == "Women") {
+      w_byevent
+    } else {
+      m_byevent
+    }
+  })
   
   #Event Table
   output$byevent <-renderDT({
     events_data() %>% filter(Event == input$eventc) %>% arrange(desc(AverageScore))
   })
   
-  #selector for events on by Event Page
+  #selector for events on By Event Page in Gymnasts Tab
   output$events_choices <- renderUI({
     selectInput("eventc", label = "Event", choices = unique(as.character(events_data()$Event)))
   })
   
+  #selectors for events on By Event Page in Gymnasts Tab
+  output$event_choices_tb1 <- renderUI({
+    selectInput("event_g1", label = "Team Events: Gymnast 1", choices = unique(as.character(events_data2()$Event)), multiple = TRUE)
+  })
+  output$event_choices_tb2 <- renderUI({
+    selectInput("event_g2", label = "Team Events: Gymnast 2", choices = unique(as.character(events_data2()$Event)), multiple = TRUE)
+  })
+  output$event_choices_tb3 <- renderUI({
+    selectInput("event_g3", label = "Team Events: Gymnast 3", choices = unique(as.character(events_data2()$Event)), multiple = TRUE)
+  })
+  output$event_choices_tb4 <- renderUI({
+    selectInput("event_g4", label = "Team Events: Gymnast 4", choices = unique(as.character(events_data2()$Event)), multiple = TRUE)
+  })
+  output$event_choices_tb5 <- renderUI({
+    selectInput("event_g5", label = "Team Events: Gymnast 5", choices = unique(as.character(events_data2()$Event)), multiple = TRUE)
+  })
+  
+  observeEvent(input$show_choices, {
+    # Create a list to store selected choices
+    selected_choices <- list()
+    
+    # Retrieve and store selected gymnasts
+    selected_choices$gymnasts <- c(
+      input$g1input, input$g2input, input$g3input, input$g4input, input$g5input
+    )
+    
+    # Retrieve and store selected events for each gymnast
+    selected_choices$events <- lapply(1:5, function(i) {
+      dropdown_id <- paste0("event_g", i)
+      input[[dropdown_id]]
+    })
+    
+    # Display selected choices
+    output$selected_choices_output <- renderText({
+      paste("Selected Gymnasts: ", paste(selected_choices$gymnasts, collapse = ", "), "\n",
+            "Selected Events:")
+      for (i in 1:5) {
+        dropdown_id <- paste0("event_g", i)
+        events_text <- paste(input[[dropdown_id]], collapse = ", ")
+        cat(paste("  Gymnast", i, ":", events_text, "\n"))
+      }
+    })
+  })
+  
+  
+  
 }
+
+
+
+
+
 
--- App-1/ui.R
+++ App-1/ui.R
@@ -16,25 +16,19 @@ ui <- navbarPage(
              p("Welcome to the USA Gymnastics Olympic Selection Tool for the Paris 2024 Olympics. This tool is designed to assist the USA Gymnastics Olympic Committee in making informed decisions about athlete selection."),
              
              # Placeholder for App Logo/Image
-             img(src = "path/to/your/logo.png", alt = "App Logo", width = 200),
+             img(src = "usagymnastics.png", alt = "App Logo"),
              
              # App Features
              h3("Key Features"),
              tags$ul(
-               tags$li("Statistical analysis visualized for athlete potential."),
-               tags$li("Insights into medal probability for athletes in specific events."),
-               tags$li("Predicted team medal performance."),
-               tags$li("Empowering the selection process for the Paris 2024 Olympics.")
+               tags$li("Statistical analysis visualized for athlete potential"),
+               tags$li("Insights into medal probability for athletes in specific events"),
+               tags$li("Predicted team medal performance"),
+               tags$li("Empowering the selection process for the Paris 2024 Olympics")
              ),
              
-             # Placeholder for Screenshots/Images
-             h3("Screenshots"),
-             div(
-               class = "screenshot-container",
-               img(src = "path/to/screenshot1.png", alt = "Screenshot 1", width = 300),
-               img(src = "path/to/screenshot2.png", alt = "Screenshot 2", width = 300),
-               # Add more screenshots as needed
-             ),
+            h3("How it works"),
+            p("The full details of our methodology can be found in our report, which can be found at https://github.com/sta440-fa23/Case2-BlueDevilSM"),
              
              # Source Data Information
              h3("Source Data"),
@@ -43,33 +37,33 @@ ui <- navbarPage(
              
              
              h3("Team | Contact Information"),
-             p("Our team consists of 3 members: A, B, C."),
+             p("Our team consists of 3 members: Ben Thorpe, Sean Li, and Chris Tsai. The three of us are all seniors at Duke University studying Statistics and part of the Duke Sports Analytics Club. You can find more information about the Duke Sports Analytics Club at https://dukesportsanalytics.com"),
              
              # Team Member A
              div(
                class = "team-member",
-               h4("Team Member A"),
+               h4("Ben Thorpe"),
                img(src = "path/to/team_member_a.jpg", alt = "Team Member A Photo", width = 100),
                p("Bio: Team Member A is an expert in gymnastics analytics with a background in statistics."),
-               p("Contact: team_member_a@usagymselectiontool.com")
+               p("Contact: benjamin.thorpe@duke.edu")
              ),
              
              # Team Member B
              div(
                class = "team-member",
-               h4("Team Member B"),
+               h4("Sean Li"),
                img(src = "path/to/team_member_b.jpg", alt = "Team Member B Photo", width = 100),
                p("Bio: Team Member B is a web development specialist with a passion for sports analytics."),
-               p("Contact: team_member_b@usagymselectiontool.com")
+               p("Contact: sean.li571@duke.edu")
              ),
              
              # Team Member C
              div(
                class = "team-member",
-               h4("Team Member C"),
+               h4("Chris Tsai"),
                img(src = "path/to/team_member_c.jpg", alt = "Team Member C Photo", width = 100),
                p("Bio: Team Member C is a data visualization expert with extensive experience in gymnastics."),
-               p("Contact: team_member_c@usagymselectiontool.com")
+               p("Contact: christopher.tsai@duke.edu")
              )
              
              
@@ -103,10 +97,38 @@ ui <- navbarPage(
   ),
   
   ### Team Builder
-  tabPanel("Team Builder", textInput("g1input", h3("Gymnast 1")),
-           textInput("g2input", h3("Gymnast 2")),
-           textInput("g3input", h3("Gymnast 3")),
-           textInput("g4input", h3("Gymnast 4")),
-           textInput("g5input", h3("Gymnast 5")))
+  tabPanel("Team Builder",
+           fluidRow(
+             column(3,
+                     #h2("Full Name"),
+                    selectInput("genderTeamBuilder", 
+                                label = "Gender",
+                                choices = list("Women", "Men"),
+                                selected = "Women"),
+                    textInput("g1input", h3("Gymnast 1")),
+                    textInput("g2input", h3("Gymnast 2")),
+                    textInput("g3input", h3("Gymnast 3")),
+                    textInput("g4input", h3("Gymnast 4")),
+                    textInput("g5input", h3("Gymnast 5")),
+                    
+             ),
+             
+             column(3,
+                    h2("Team Events"),
+                    h2("\n"), #i'm trying to space things out better but this \n doesn't do anything? 
+                    uiOutput("event_choices_tb1"),
+                    uiOutput("event_choices_tb2"),
+                    uiOutput("event_choices_tb3"),
+                    uiOutput("event_choices_tb4"),
+                    uiOutput("event_choices_tb5"),
+                    actionButton("show_choices", "Show Choices")
+                    
+                    
+             ),
+             textOutput("selected_choices_output")
+             
+             
+           )
+  )
   
 )
